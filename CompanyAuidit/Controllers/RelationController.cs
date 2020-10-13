ecf396b0df4c8f14a805c3b11c77025be467e3d6
Furkan ZEREY <ybs.furkanzerey@gmail.com>
2020-10-13 16:41:07 +0300

    Sayfa düzenlemeleri yapıldı. Kişi listesine ilişki sayfası için bağlantı sağlandı.

--- CompanyAuidit/Controllers/RelationController.cs
+++ CompanyAuidit/Controllers/RelationController.cs
@@ -52,7 +52,7 @@ namespace CompanyAuidit.Controllers
             var result = _context.UserAndInventoriyRelationship.FirstOrDefault(x => x.UserId == userId && x.InventoriyId == inventoriyId);
             _context.UserAndInventoriyRelationship.Remove(result);
             _context.SaveChanges();
-            return RedirectToAction("Index", "User");
+            return RedirectToAction("UserList","User");
         }
 
         public IActionResult CreateItem(int userId, int itemId)
@@ -60,7 +60,7 @@ namespace CompanyAuidit.Controllers
             var result = new UserAndInventoriyRelationship { UserId = userId, InventoriyId = itemId };
             _context.UserAndInventoriyRelationship.Add(result);
             _context.SaveChanges();
-            return RedirectToAction("Index", "User");
+            return RedirectToAction("UserList", "User");
         }
 
     }
